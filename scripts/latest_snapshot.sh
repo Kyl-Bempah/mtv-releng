e39643cabe8d20ae29e1e27c3db0a507e26c5551
Stefan Olenocin <solenoci@redhat.com>
2025-07-02 23:54:36 +0200

    Include misc scripts
    Signed-off-by: Stefan Olenocin <solenoci@redhat.com>

---
+++ scripts/latest_snapshot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Gets the latest successful snapshot for specified version of forklift, e.g. forklift-operator-2-9...
+
+version=$1
+
+# Print Usage if argument is missing
+if [[ -z $1 ]]; then
+    echo -e "Gets the latest successful snapshot for specified version of forklift, e.g. forklift-operator-2-9...\n"
+    echo "Usage: ./latest_snapshot.sh <version>, examples:"
+    echo "./latest_snapshot.sh 2-9"
+    echo "./latest_snapshot.sh dev-preview"
+    exit 0
+fi
+
+
+oc get snapshots --sort-by='{.metadata.creationTimestamp}' -o custom-columns=NAME:.metadata.name | grep $version | tac | head -n 1
---
+++ scripts/snapshot_content.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+snapshot=$1
+
+# Print Usage if argument is missing
+if [[ -z $1 ]]; then
+    echo "Usage: ./snapshot_content.sh <snapshot name>, examples:"
+    echo "./snapshot_content.sh forklift-operator-2-9-ffrxv"
+    exit 0
+fi
+
+oc get -o=json snapshot $snapshot | jq -e '.spec.components | map({ name, containerImage}) | sort'
