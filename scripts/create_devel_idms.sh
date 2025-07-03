e750ecc6ab49d8ba19af25e614eef5d9ea63e67e
Stefan Olenocin <solenoci@redhat.com>
2025-07-03 14:29:26 +0200

    Update create_devel_imds
    Signed-off-by: Stefan Olenocin <solenoci@redhat.com>

--- scripts/create_devel_idms.sh
+++ scripts/create_devel_idms.sh
@@ -2,49 +2,75 @@
 
 # Applies IDMS for testing the MTV in a cluster before release
 
+version=$1
+
+# Print Usage if argument is missing
+if [[ -z $1 ]]; then
+    echo "Usage: ./create_devel_idms.sh <version>, examples:"
+    echo "./create_devel_idms.sh 2-9"
+    echo "./create_devel_idms.sh dev-preview"
+    exit 0
+fi
+
 cat << EOF > devel-idms.yaml
 apiVersion: config.openshift.io/v1
 kind: ImageDigestMirrorSet
 metadata:
-  name: devel-testing
+  name: devel-testing-for-${version}
 spec:
   imageDigestMirrors:
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-controller-rhel9
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/forklift-controller-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-controller-rhel9
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-api-rhel9
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/forklift-api-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-api-rhel9
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-must-gather-rhel8
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/forklift-must-gather-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-must-gather-rhel8
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-console-plugin-rhel9
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/forklift-console-plugin-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-console-plugin-rhel9
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-validation-rhel9
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/validation-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-validation-rhel9
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-virt-v2v-rhel9
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/virt-v2v-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-virt-v2v-rhel9
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-populator-controller-rhel9
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/populator-controller-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-populator-controller-rhel9
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-rhv-populator-rhel8
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/ovirt-populator-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-rhv-populator-rhel8
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-openstack-populator-rhel9
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/openstack-populator-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-openstack-populator-rhel9
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-ova-provider-server-rhel9
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/ova-provider-server-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-ova-provider-server-rhel9
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-vsphere-xcopy-volume-populator-rhel9
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/vsphere-xcopy-volume-populator-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-vsphere-xcopy-volume-populator-rhel9
     - mirrors:
         - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-rhel9-operator
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/forklift-operator-${version}
       source: registry.redhat.io/migration-toolkit-virtualization/mtv-rhel9-operator
+    - mirrors:
+        - registry.stage.redhat.io/migration-toolkit-virtualization/mtv-operator-bundle
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/forklift-operator-bundle-${version}
+      source: registry.redhat.io/migration-toolkit-virtualization/mtv-operator-bundle
 EOF
 
 oc apply -f devel-idms.yaml
