32bbe42c48c756bdd61be3c6a5f7fdc357bbc28b
Stefan Olenocin <solenoci@redhat.com>
2025-07-23 17:55:45 +0200

    Add few useful scripts
    Signed-off-by: Stefan Olenocin <solenoci@redhat.com>

---
+++ scripts/rebuild_bundle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+version=$1
+
+# Print Usage if argument is missing
+if [[ -z $1 ]]; then
+    echo "Usage: "
+    echo "./rebuild_all.sh 2-9"
+    echo "./rebuild_all.sh dev-preview"
+    exit 0
+fi
+
+oc annotate components/forklift-operator-bundle-$version build.appstudio.openshift.io/request=trigger-pac-build
