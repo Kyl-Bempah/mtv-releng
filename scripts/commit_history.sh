f0b13a2d71ea67677016a6dcc4aff909d513bc7f
Stefan Olenocin <solenoci@redhat.com>
2025-07-30 18:58:06 +0200

    Add scripts to create a commit diff of versions
    Signed-off-by: Stefan Olenocin <solenoci@redhat.com>

--- scripts/commit_history.sh
+++ scripts/commit_history.sh
@@ -8,11 +8,11 @@ forklift_must_gather_url="git@github.com:kubev2v/forklift-must-gather.git"
 
 # Print Usage if argument is missing
 if [[ -z $1 || -z $2 || -z $3 || -z $4 ]]; then
-    echo -e "Gets commit history for specified component (backend, frontend or gather), for specified branch (main, release-2.9...) and from the most recent commit to oldest commit (excluding the oldest commit in output)\n"
+    echo -e "Gets commit history for specified component (forklift, forklift-console-pluign or forklift-must-gather), for specified branch (main, release-2.9...) and from the most recent commit to oldest commit (excluding the oldest commit in output)\n"
     echo "Usage: ./commit_history.sh <component> <branch> <from_commit> <to_commit>, examples:"
-    echo "./commit_history.sh backend main 881e5d25da866700d9d68b5a6dc109cd8ddbaa39 600e32ad5327240e94433a219d5269d3e12e8a4d"
-    echo "./commit_history.sh frontend main d6cbce8e6e1cb7e21be8d25d36a2c1310d2f4bcc 9dc23ebcfd1941bd5dc2e431a0d349653ddc1f85"
-    echo "./commit_history.sh gather release-2.9 c4ee6c5acb9e837ce97a391e85b90d2752546a1f 8c0e1ff419eac68726f5a7d3dcd279036fe92b3c"
+    echo "./commit_history.sh forklift main 881e5d25da866700d9d68b5a6dc109cd8ddbaa39 600e32ad5327240e94433a219d5269d3e12e8a4d"
+    echo "./commit_history.sh forklift-console-pluign main d6cbce8e6e1cb7e21be8d25d36a2c1310d2f4bcc 9dc23ebcfd1941bd5dc2e431a0d349653ddc1f85"
+    echo "./commit_history.sh forklift-must-gather release-2.9 c4ee6c5acb9e837ce97a391e85b90d2752546a1f 8c0e1ff419eac68726f5a7d3dcd279036fe92b3c"
     exit 0
      
 fi
@@ -25,15 +25,9 @@ to=$4
 mkdir temp
 cd temp
 git init &> /dev/null
-if [[ $c == "backend" ]]; then
-    git remote add origin $forklift_url
-elif [[ $c == "frontend" ]]; then
-    git remote add origin $forklift_console_plugin_url
-elif [[ $c == "gather" ]]; then
-    git remote add origin $forklift_must_gather_url
-fi
+git remote add origin https://github.com/kubev2v/$c.git
 git fetch origin $branch &> /dev/null
-git log --remotes --format=format:'%H%n  Commit: %s%n  Author: %an' --grep="chore(.*)" --invert-grep $from...$to > history
+git log --remotes --format=format:'%H%n  Commit: %s%n  Author: %an' --grep="chore(.*)" --invert-grep $from..$to > history
 echo $'' >> history
 cat history
 cd ..
---
+++ scripts/replace_for_quay.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Replaces prod and stage image urls with quay urls
+
+img=$1
+version=$2
+
+# Print Usage if img URL or version is missing 
+if [[ -z $1 || -z $2 ]]; then
+    echo "Usage: ./replace_for_quay.sh <url to img> <version>, examples:"
+    echo "./replace_for_quay.sh registry.stage.redhat.io/migration-toolkit-virtualization/mtv-operator-bundle@sha256:165142389fd7af082eea8873f24317199462376f2b600c871c1e30b63fef0295 2.9.1"
+    echo "./replace_for_quay.sh registry.redhat.io/mtv-candidate/mtv-operator-bundle@sha256:165142389fd7af082eea8873f24317199462376f2b600c871c1e30b63fef0295 2.10.0"
+    echo "./replace_for_quay.sh registry.redhat.io/migration-toolkit-virtualization/mtv-operator-bundle@sha256:165142389fd7af082eea8873f24317199462376f2b600c871c1e30b63fef0295 2.8.6"
+    exit 0
+fi
+
+# if image is not from redhat.io or stage.redhat.io exit
+if ! [[ "$img" == *"redhat.io"* ]]; then
+  echo $img
+  exit 0
+fi
+
+# check version, quay images are only supported after 2.8.6 (including)
+if [ "$(printf '%s\n' "$version" "2.8.5" | sort -rV | head -n 1)" == "2.8.5" ]; then 
+  echo $img
+  exit 0
+fi
+
+
+cmps='{"mtv-controller-rhel9": "forklift-controller", "mtv-must-gather-rhel8": "forklift-must-gather", "mtv-validation-rhel9":"validation", "mtv-api-rhel9":"forklift-api", "mtv-populator-controller-rhel9":"populator-controller", "mtv-rhv-populator-rhel8":"ovirt-populator", "mtv-virt-v2v-rhel9":"virt-v2v", "mtv-openstack-populator-rhel9":"openstack-populator", "mtv-console-plugin-rhel9":"forklift-console-plugin", "mtv-ova-provider-server-rhel9":"ova-provider-server", "mtv-vsphere-xcopy-volume-populator-rhel9":"vsphere-xcopy-volume-populator", "mtv-rhel9-operator":"forklift-operator", "mtv-operator-bundle": "forklift-operator-bundle"}'
+
+# get only the image name
+IFS='/'
+read -a split_img <<< $img
+cmp=${split_img[-1]}
+repo=${split_img[1]}
+IFS='@'
+read -a split_cmp <<< $cmp
+cmp=${split_cmp[0]}
+sha=${split_cmp[-1]}
+IFS=''
+
+# pair the prod cmp name with quay cmp name
+cmp=$(echo $cmps | jq ".\"$cmp\"")
+# strip quotes
+cmp=${cmp:1:-1}
+
+# for mtv-candidate repo, the version is always dev-preview
+if [[ $repo == "mtv-candidate" ]]; then
+  cmp_ver="dev-preview"
+else
+  # get only x-y from x.y.z version and replace dot for dash
+  IFS='.'
+  read -a split_v <<< $version
+  IFS=''
+
+  cmp_ver=${split_v[0]}-${split_v[1]}
+fi
+
+registry="quay.io/redhat-user-workloads/rh-mtv-1-tenant"
+echo $registry"/"forklift-operator-$cmp_ver"/"$cmp"-"$cmp_ver"@"$sha
+
