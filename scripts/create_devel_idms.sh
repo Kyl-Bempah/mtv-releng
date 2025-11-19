6322489276f4ddcd0c8495ca79d6ae425329c9e9
Stefan Olenocin <solenoci@redhat.com>
2025-11-19 13:11:17 +0100

    Keep also the old RHEL version
    Signed-off-by: Stefan Olenocin <solenoci@redhat.com>

--- scripts/create_devel_idms.sh
+++ scripts/create_devel_idms.sh
@@ -50,8 +50,12 @@ spec:
       source: registry.redhat.io/${repo}/mtv-validation-rhel9
     - mirrors:
         - registry.stage.redhat.io/${repo}/mtv-virt-v2v-rhel10
-        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/virt-v2v-int-${version}
+        - quay.io/redhat-user-workloads/rh-mtv-btrfs-tenant/forklift-operator-int-${version}/virt-v2v-int-${version}
       source: registry.redhat.io/${repo}/mtv-virt-v2v-rhel10
+    - mirrors:
+        - registry.stage.redhat.io/${repo}/mtv-virt-v2v-rhel9
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/virt-v2v-${version}
+      source: registry.redhat.io/${repo}/mtv-virt-v2v-rhel9
     - mirrors:
         - registry.stage.redhat.io/${repo}/mtv-populator-controller-rhel9
         - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/populator-controller-${version}
@@ -103,7 +107,10 @@ spec:
         - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/validation-${version}
       source: registry.stage.redhat.io/${repo}/mtv-validation-rhel9
     - mirrors:
-        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/virt-v2v-int-${version}
+        - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/virt-v2v-${version}
+      source: registry.stage.redhat.io/${repo}/mtv-virt-v2v-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/rh-mtv-btrfs-tenant/forklift-operator-int-${version}/virt-v2v-int-${version}
       source: registry.stage.redhat.io/${repo}/mtv-virt-v2v-rhel10
     - mirrors:
         - quay.io/redhat-user-workloads/rh-mtv-1-tenant/forklift-operator-${version}/populator-controller-${version}
